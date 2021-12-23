b9d4e84174403b15ebfcb4026ff8fa3573c0df42
Ahmed Samir <ahmedsamir551@gmail.com>
2021-12-24 01:47:34 +0200

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,6 +4,9 @@ const HomePage = () => (
   <div>
     <h1>Welcome</h1>
     <p>This is my site. Take a look around!</p>
+      <br/>
+    <h3>Check my advanced version</h3>
+    <a href="https://ahmedsamir.dev/" target="_blank">[My Portfolio]</a>
   </div>
 );
 
