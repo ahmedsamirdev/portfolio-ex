511916325c56fe1026c229e17d9f0275ce68a3bc
Ahmed Samir <ahmedsamir551@gmail.com>
2021-12-24 01:43:40 +0200

    Update PortfolioPage.js

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -7,9 +7,9 @@ const PortfolioPage = () => (
     <p>Checkout the stuff I've done:</p>
     <Link to="/portfolio/1">Item One</Link>
     <Link to="/portfolio/2">Item Two</Link>
-    <br>
-    <br>
+
     <h3>Check my advanced version</h3>
+    <a href="https://ahmedsamir.dev/" target=”_blank”>[My portfolio]</a>
   </div>
 );
 
